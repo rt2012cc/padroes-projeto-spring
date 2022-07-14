803522cf8d92ddbdac78eee2cde84f669bd7c72e
Renan <rt2012cc@gmail.com>
2022-07-14 11:55:10 -0400

    first commit

---
+++ src/main/java/com/renan/padroesprojetospring/service/ClienteService.java
@@ -0,0 +1,13 @@
+package com.renan.padroesprojetospring.service;
+
+import com.renan.padroesprojetospring.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+    Cliente buscarPorId(Long id);
+    void inserir(Cliente cliente);
+    void atualizar(Long id, Cliente cliente);
+    void deletar(Long id);
+
+}
